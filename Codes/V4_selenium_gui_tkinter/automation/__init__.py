9f22d4c9c4bf6242958d5d1e47060d4026e98c86
Bingqiang Zhou <1299050656@qq.com>
2026-02-23 12:51:32 +0800

    V3: Move V3 files to codes root and remove V1, V2, V4 folders

--- Codes/V4_selenium_gui_tkinter/automation/__init__.py
+++
@@ -1,6 +0,0 @@
-# Automation package
-from .form_filler import FormFiller
-from .verification import VerificationHandler
-from .browser_setup import BrowserSetup
-
-__all__ = ['FormFiller', 'VerificationHandler', 'BrowserSetup']
--- Codes/V4_selenium_gui_tkinter/views/__init__.py
+++
@@ -1,9 +0,0 @@
-# Views package
-from .main_view import MainView
-from .fill_view import FillView
-from .analyze_view import AnalyzeView
-from .rule_editor_view import RuleEditorView
-from .history_view import HistoryView
-from .loading_window import LoadingWindow
-
-__all__ = ['MainView', 'FillView', 'AnalyzeView', 'RuleEditorView', 'HistoryView', 'LoadingWindow']
